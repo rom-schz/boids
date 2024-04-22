d121c0504ada6fdb0e6025619e6aa67b573189db
Romain Schwarz <rosch4214@gmail.com>
2024-04-22 19:19:21 +0200

    Simple boid architecture - first version.

--- include/boid.hpp
+++ include/boid.hpp
@@ -5,22 +5,25 @@
 
 class Boid {
 public:
-	Boid();
-	Boid(std::vector<float> pos, std::vector<float> dir, std::vector<float> acc, float mass=1.0, float range=1.0, float fov=1.57);
-	Boid(std::vector<float> pos, std::vector<float> dir, float mass=1.0, float range=1.0, float fov=1.57);
-	Boid(std::vector<float> pos, float mass=1.0, float range=1.0, float fov=1.57);
+	Boid(float x, float y, float vx, float vy);
 	~Boid()=default;
 
+	void update();
 
 private:
-	std::vector<float> pos;
-	std::vector<float> vel;
-	std::vector<float> acc;
+	float x; float y;
+	float vx; float vy;
 
-	float mass;
+	float maxSpeed;
+	float minSpeed;
 
-	float range; //range of view
-	float fov; //field of view
+	float avoidFactor;
+	float centeringFactor;
+	float matchingFactor;
+
+	void separation();
+	void alignment();
+	void cohesion();
 };
 
 #endif
--- src/boid.cpp
+++ src/boid.cpp
@@ -1,3 +1,19 @@
 #include "boid.hpp"
 
+Boid::Boid(float x, float y, float vx, float vy) {
+	this->x = x;
+	this->y = y;
+	this->vx = vx;
+	this->vy = vy;
+}
 
+
+void Boid::update() {
+	this->separation();
+	this->alignment();
+	this->cohesion();
+}
+
+void Boid::separation() {
+	
+}
